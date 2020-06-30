29a6c3a694920cd3305ac98a0509d0ed36a0ef39
Stefan <stefan.cosquer@gmail.com>
2020-06-30 17:36:54 +0200

    added absolute path fo ruse with npx

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ const app = express();
 
 app.use(bodyParser.json())
 
-fs.readdir("api", (err, files) => {
+fs.readdir(`${__dirname}/api`, (err, files) => {
   files.forEach(file => {
     const match = /^(.*)\.js$/.exec(file);
     if (match) {
       const url = `/api/${match[1]}`;
-      app.use(url, require(`./api/${file}`));
+      app.use(url, require(`${__dirname}/api/${file}`));
       process.stdout.write(`- created route ${url}\n`);
     }
   });
